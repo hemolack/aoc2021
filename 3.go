4b492d0b70e582ed7985ddd8cc2fd7de8f6a88ec
hemolack <dr_moreau@comcast.net>
2021-12-03 16:03:23 -0500

    Finished day 3

--- 3.go
+++ 3.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -10,7 +9,7 @@ import (
 
 func main() {
 	pwd, _ := os.Getwd()
-	file, err := os.Open(pwd + "\\3b.txt")
+	file, err := os.Open(pwd + "\\3.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,20 +50,17 @@ func main() {
 		}
 	}
 	o2_rating, _ := strconv.ParseInt(get_o2_rating(line, o2_numbers, bits), 2, 64)
-	// co2_rating, _ := strconv.ParseInt(get_co2_rating(line, co2_numbers, bits), 2, 64)
-	fmt.Println(bits)
+	co2_rating, _ := strconv.ParseInt(get_co2_rating(line, co2_numbers, bits), 2, 64)
 	println("O2 Generator Rating: ", o2_rating)
-	// println("CO2 Generator Rating: ", co2_rating)
+	println("CO2 Generator Rating: ", co2_rating)
 	println("Gamma: ", gamma)
 	println("Epsilon: ", epsilon)
 	println("Power consumption: ", gamma*epsilon)
-	// println("Life support rating: ", o2_rating*co2_rating)
+	println("Life support rating: ", o2_rating*co2_rating)
 }
 
 func most_used(line int, position int, bits []int) string {
-	// println("*", line, position, bits[position])
 	if line-bits[position] > bits[position] {
-		// println(line, "-", bits[position], ">", bits[position], "at position", position)
 		return "0"
 	} else {
 		return "1"
@@ -72,9 +68,7 @@ func most_used(line int, position int, bits []int) string {
 }
 
 func least_used(line int, position int, bits []int) string {
-	// println("*", line, position, bits[position])
 	if line-bits[position] <= bits[position] {
-		// println(line, "-", bits[position], "<=", bits[position], "at position", position)
 		return "0"
 	} else {
 		return "1"
@@ -83,24 +77,20 @@ func least_used(line int, position int, bits []int) string {
 
 func get_o2_rating(line int, o2_numbers []string, bits []int) string {
 	bit_length := len(o2_numbers[0])
-	fmt.Println(o2_numbers)
 	for i := 0; i < bit_length; i++ {
 		most_used := most_used(len(o2_numbers), i, bits)
 		o2_numbers = remove_numbers(o2_numbers, i, most_used)
 		if len(o2_numbers) == 1 {
-			println("FOUND O2 NUMBER")
 			return o2_numbers[0]
 		}
 		bits = get_bitmap(o2_numbers)
 	}
-	println("FAILED, TOO MANY O2 NUMBERS", len(o2_numbers))
 	return o2_numbers[0]
 }
 
 func get_co2_rating(line int, co2_numbers []string, bits []int) string {
 	bit_length := len(co2_numbers[0])
 	for i := 0; i < bit_length; i++ {
-		fmt.Println("position", i, "bits", bits, "for", len(co2_numbers), co2_numbers)
 		least_used := least_used(len(co2_numbers), i, bits)
 		co2_numbers = remove_numbers(co2_numbers, i, least_used)
 		if len(co2_numbers) == 1 {
@@ -138,19 +128,10 @@ func remove_numbers(numbers []string, position int, required string) []string {
 		}
 		bit := string([]rune(numbers[i])[position])
 		if bit != required {
-			println("removing", numbers[i], "because bit", position, "is not", required, "i = ", i)
-			if position == len(numbers[i])-1 {
-				numbers = append(numbers[:position])
-				break
-			} else if position == 0 {
-				numbers = numbers[1:]
-			} else {
-				numbers = append(numbers[:i], numbers[i+1:]...)
-				continue
-			}
+			numbers = append(numbers[:i], numbers[i+1:]...)
+			continue
 		}
 		i++
 	}
-	fmt.Println("#", numbers)
 	return numbers
 }
